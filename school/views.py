49fe17cee85fabc714a55f95ca3305ad396bb174
Rupak Khadka <rupak.fcc@gmail.com>
2022-04-19 12:21:19 +0545

    third

--- school/views.py
+++ school/views.py
@@ -1,4 +1,5 @@
-from optparse import Values
+
+from datetime import datetime
 from django.shortcuts import render
 from .models import Student
 
@@ -7,8 +8,14 @@ from .models import Student
 def home(request):
     # student_data = Student.objects.get(id=1)
     # student_data = Student.objects.first()
-    student_data = Student.objects.order_by('name').first()
-    print("Return :", student_data)
+    # student_data = Student.objects.order_by('name').first()
+    # student_data = Student.objects.last()
+    # student_data = Student.objects.latest('city')
+    # student_data = Student.objects.earliest('city')
+    student_data = Student.objects.all()
+    Student.objects.create(name='Ram',roll=150,city='thn',marks=99,passing_year=datetime(2022,12,5))
+    print(student_data.exists())
+    # print("Return :", student_data)
     print()
     
     return render(request,'school/home.html',{'students':student_data})
